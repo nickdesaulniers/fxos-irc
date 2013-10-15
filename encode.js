fb9f6c2d6a667dc1dc67a3c6cbb29f9b24b3c883
Nick Desaulniers <ndesaulniers@mozilla.com>
2013-10-15 15:52:19 -0700

    small encode optimizations

--- encode.js
+++ encode.js
@@ -7,7 +7,7 @@ var Utf8 = {
     for (n = 0, len = string.length; n < len; ++n) {
       c = string.charCodeAt(n);
       if (c < 128) {
-        utftext += String.fromCharCode(c);
+        utftext += string[n];
       } else if (c > 127 && c < 2048) {
         utftext += String.fromCharCode((c >> 6) | 192);
         utftext += String.fromCharCode((c & 63) | 128);
@@ -21,21 +21,20 @@ var Utf8 = {
   },
   decode: function (utftext) {
     var string = "";
-    var i = 0;
-    var c = c1 = c2 = 0;
-    while (i < utftext.length) {
+    var i = c = c1 = c2 = 0;
+    var len = utftext.length;
+    while (i < len) {
       c = utftext.charCodeAt(i);
       if (c < 128) {
-        string += String.fromCharCode(c);
-        i++;
+        string += utftext[i++];
       } else if (c > 191 && c < 224) {
-        c2 = utftext.charCodeAt(i + 1);
-        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+        c1 = utftext.charCodeAt(i + 1);
+        string += String.fromCharCode(((c & 31) << 6) | (c1 & 63));
         i += 2;
       } else {
-        c2 = utftext.charCodeAt(i + 1);
-        c3 = utftext.charCodeAt(i + 2);
-        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+        c1 = utftext.charCodeAt(i + 1);
+        c2 = utftext.charCodeAt(i + 2);
+        string += String.fromCharCode(((c & 15) << 12) | ((c1 & 63) << 6) | (c2 & 63));
         i += 3;
       }
     }
